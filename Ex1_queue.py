111072ec0147adf0962380c58a91d7f3aa22fa36
Sahrial Ihsani Ishak <67180039+sahrialihsani@users.noreply.github.com>
2021-05-19 05:41:20 +0700

    Create Ex1_queue.py

---
+++ Ex1_queue.py
@@ -0,0 +1,60 @@
+class Node :
+    def __init__(self,data=None,next_node=None):
+        self.data = data
+        self.next_node = next_node
+    
+    def get_data(self):
+        return self.data
+    
+    def get_next(self):
+        return self.next_node
+    
+    def set_next(self,new_node):
+        self.next_node = new_node
+        
+class Queue :
+    def __init__(self,head=None):
+        self.head = head
+    
+    def enqueue(self,data):
+        new_item = Node(data)
+        current = self.head
+        if current is None:
+            self.head = new_item
+        else:
+            while current.get_next():
+                current = current.get_next()
+            current.set_next(new_item)
+    def dequeue(self):
+        current = self.head
+        if current != None:
+            self.head = current.get_next()
+        else:
+            print("Queue is empty.")
+
+    def __len__(self):
+        return len(self.temp)
+
+    def print_queue(self):
+        current = self.head
+        self.temp = []
+        while current:
+            self.temp.append(current.get_data())
+            current = current.get_next()
+        print(self.temp)
+    
+q = Queue()
+
+q.enqueue("User01")
+q.enqueue("User02")
+q.enqueue("User03")
+q.enqueue("User04")
+q.enqueue("User05")
+
+q.print_queue()
+
+q.dequeue()
+q.print_queue()
+
+q.dequeue()
+q.print_queue()
