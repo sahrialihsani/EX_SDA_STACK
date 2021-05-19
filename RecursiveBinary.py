68704d42cf7d40da84ce9d71602224390a143e13
Sahrial Ihsani Ishak <67180039+sahrialihsani@users.noreply.github.com>
2021-05-19 07:50:39 +0700

    Add files via upload

---
+++ RecursiveBinary.py
@@ -0,0 +1,34 @@
+
+#Rekursif Binary Search 
+def binary_search(array, kiri, kanan, nilaidicari):
+ 
+    # cek base
+    if kanan >= kiri:
+ 
+        tengah = (kanan + kiri) // 2
+ 
+        # Jika elemen ada di tengah
+        if array[tengah] == nilaidicari:
+            return tengah
+ 
+        # Jika elemen lebih kiri dari tengah, elemen berada di sebelah kiri
+        elif array[tengah] > nilaidicari:
+            return binary_search(array, kiri, tengah - 1, nilaidicari)
+ 
+        # Jika tidak, elemen hanya bisa ada di sebelah kanan
+        else:
+            return binary_search(array, tengah + 1, kanan, nilaidicari)
+ 
+    else:
+        # Elemen tidak ada di arrayay
+        return -1
+ 
+# Masukan data pada berbentuk arrayay
+array = [ 2, 3, 4, 10, 40 ]
+nilaidicari = 10
+# Lakukan pencarian
+result = binary_search(array, 0, len(array)-1, nilaidicari)
+if result != -1:
+    print("Elemen berada pada indeks ke: ", str(result))
+else:
+    print("Elemen tidak berada didalam arrayay")
